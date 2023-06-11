f602535b539a35600420aff19e2b25c3cb1ef4e4
Fernando Ramos <fndo.ramos@gmail.com>
2023-06-11 22:08:10 +0200

    add functionality to display dives of an user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,20 +4,29 @@ const router = express.Router();
 const Dive = require("../models/Dive.model");
 const User = require("../models/User.model");
 
-
 router.get("/user-profile/:username", (req, res, next) => {
-    const username = req.session.currentUser.username;
+  const username = req.session.currentUser.username;
+  let userIsLoggedIn = null;
+  let userDivesArr = [];
 
-    User.findOne({username: username})
-        .then((userIsLoggedIn) => {
-            res.render("users/user-profile", { userIsLoggedIn })
-        })
-        .catch((error) => {
-            console.log("Error finding user in DB", error);
-            next(error);
-        });
+  User.findOne({ username: username })
+    .then((userDBIsLoggedIn) => {
+      userIsLoggedIn = userDBIsLoggedIn;
+      return Dive.find().populate("user");
+    })
+    .then((allDives) => {
+      allDives.forEach((dive) => {
+        if (dive.user.username === username) {
+          userDivesArr.push(dive);
+        }
+      });
+
+      res.render("users/user-profile", { userDivesArr, userIsLoggedIn });
+    })
+    .catch((error) => {
+      console.log("Error finding user in DB", error);
+      next(error);
+    });
 });
-
-
 
 module.exports = router;
