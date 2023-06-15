e7e0deb41ebb9c151652e859ab9f0d783ff91691
Fernando Ramos <fndo.ramos@gmail.com>
2023-06-15 19:25:32 +0200

    move cloudinary keys to .env

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -7,17 +7,17 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-// cloudinary.config({
-// 	cloud_name: process.env.CLOUDINARY_NAME,
-// 	api_key: process.env.CLOUDINARY_KEY,
-// 	api_secret: process.env.CLOUDINARY_SECRET,
-// });
-
 cloudinary.config({
-	cloud_name: "dcslof4ax",
-	api_key: "271992846632912",
-	api_secret: "-9HsGXBCjCgsXoNrBspfrW8h2zU",
+cloud_name: process.env.CLOUDINARY_NAME,
+api_key: process.env.CLOUDINARY_KEY,
+api_secret: process.env.CLOUDINARY_SECRET,
 });
+
+// cloudinary.config({
+// 	cloud_name: "dcslof4ax",
+// 	api_key: "271992846632912",
+// 	api_secret: "-9HsGXBCjCgsXoNrBspfrW8h2zU",
+// });
 
 const storage = new CloudinaryStorage({
 	// cloudinary: cloudinary,
